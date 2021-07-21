10db61bb8b4ccc5c09ccf3b44f7c06a54415a9cc
Artem <a.moiseyenko95@gmail.com>
2021-07-21 16:47:52 +0300

    add functions

--- project1/script.js
+++ project1/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms; 
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+        while( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+            numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+        
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,13 +21,53 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Сколько фильмов вы уже посмотрели?', ''),
-      b = prompt('НА сколько оцените его?', ''),
-      c = prompt('Сколько фильмов вы уже посмотрели?', ''),
-      d = prompt('НА сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 
-console.log(personalMovieDB);
+function rememberMyFilms () {
+    for(let i = 0; i < 2; i++){
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if(a != null && b != null && a != '' && b != '' && a.length < 50 ){
+            personalMovieDB.movies[a] = b;
+    
+        }else {
+            i--;
+        }
+    }
+}
+
+
+function writeYouGenres (){
+    for(let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYouGenres ();
+
+
+rememberMyFilms();
+
+
+function detecPersonalLevel() {
+    if(personalMovieDB.count < 10) {
+        console.log('Вероятно, предпочитаете фильмам иные развлечения');
+    }else if(personalMovieDB.count >= 10 || personalMovieDB.count < 30) { 
+        console.log('Вы классический зритель');
+    }else if(personalMovieDB.count > 50) {
+        console.log('Вы киноман!');
+    }else{
+        console.log('Пройдите опрос заново!');
+    }
+}
+
+detecPersonalLevel();
+
+function shoyMyDB (hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+shoyMyDB(personalMovieDB.privat);
